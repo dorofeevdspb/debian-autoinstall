af2a6ad2d41becc49904baefcf0ad60548943d5d
Daniel Dorofeev <dorofeev.d.spb@gmail.com>
2025-03-15 14:09:48 +0300

    deleted:    _SCRIPTS/iptables.setup.sh 	modified:   _SCRIPTS/postinstall.sh 	modified:   preseed.cfg 	modified:   settings.conf

--- _SCRIPTS/postinstall.sh
+++ _SCRIPTS/postinstall.sh
@@ -4,16 +4,16 @@
 
 # 1. Setup APT
 
-CODENAME='buster'
-PACKAGES_LIST="python3 python3-pip zabbix-agent openssh-server telnet qemu-guest-agent open-vm-tools"
+CODENAME='stable'
+PACKAGES_LIST="git ssh wget curl mc net-tools"
 WORKDIR='/root/_SCRIPTS/'
 echo -e "
 # $CODENAME
-deb http://deb.debian.org/debian/ $CODENAME main contrib non-free
-deb http://security.debian.org/debian-security $CODENAME/updates main contrib non-free
+deb http://deb.debian.org/debian/ $CODENAME main contrib non-free non-free-firmware 
+deb http://security.debian.org/debian-security $CODENAME/updates main contrib non-free non-free-firmware 
 
 # $CODENAME-updates, previously known as 'volatile'
-deb http://deb.debian.org/debian/ $CODENAME-updates main contrib non-free
+deb http://deb.debian.org/debian/ $CODENAME-updates main contrib non-free non-free-firmware 
 " > /etc/apt/sources.list
 
 printf 'Package: *\nPin: release a=testing\nPin-Priority: 90\n' > /etc/apt/preferences.d/limit-testing
@@ -28,29 +28,29 @@ apt-get -y install apt-transport-https
 
 echo -e "
 # $CODENAME
-deb https://deb.debian.org/debian/ $CODENAME main contrib non-free
-deb http://security.debian.org/debian-security $CODENAME/updates main contrib non-free
+deb https://deb.debian.org/debian/ $CODENAME main contrib non-free non-free-firmware 
+deb http://security.debian.org/debian-security $CODENAME/updates main contrib non-free non-free-firmware 
 
 # $CODENAME-updates, previously known as 'volatile'
-deb https://deb.debian.org/debian/ $CODENAME-updates main contrib non-free
+deb https://deb.debian.org/debian/ $CODENAME-updates main contrib non-free non-free-firmware 
 
 # TESTING
 deb https://deb.debian.org/debian/ testing main contrib non-free
 # Sid
 deb https://deb.debian.org/debian/ sid main contrib non-free
 # SOURCES
-#deb-src https://deb.debian.org/debian/ testing main contrib non-free 
-#deb-src https://deb.debian.org/debian/ stretch main contrib non-free
-#deb-src https://security.debian.org/debian-security stretch/updates main contrib non-free
-#deb-src https://deb.debian.org/debian/ stretch-updates main contrib non-free
+#deb-src https://deb.debian.org/debian/ stable main contrib non-free 
+#deb-src https://deb.debian.org/debian/ stable main contrib non-free
+#deb-src https://security.debian.org/debian-security stable/updates main contrib non-free
+#deb-src https://deb.debian.org/debian/ stable-updates main contrib non-free
 #deb-src https://deb.debian.org/debian/ stable main contrib non-free
 #deb-src https://security.debian.org/debian-security stable/updates main contrib non-free
 #deb-src https://deb.debian.org/debian/ stable-updates main contrib non-free
 
 " > /etc/apt/sources.list
 # 2. Zabbix agent install
-wget https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1+buster_all.deb
-apt-get update 
+#wget https://repo.zabbix.com/zabbix/4.4/debian/pool/main/z/zabbix-release/zabbix-release_4.4-1+buster_all.deb
+#apt-get update 
 
 # Packages install
 for package in $PACKAGES_LIST
@@ -59,6 +59,6 @@ do
     apt-get -y install $package 2>> $WORKDIR/install.log
 done
 
-systemctl enable zabbix-agent 
+#systemctl enable zabbix-agent 
 
 exit 0
